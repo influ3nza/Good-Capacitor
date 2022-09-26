935a940682e31470c36a025a960807a82128bf39
influ3nza <tim20021115@163.com>
2022-09-26 18:08:20 +0800

    script

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.bilibili.com/video/BV1Da411Z7s7/?spm_id_from=333.337.search-card.all.click&vd_source=dbd37b2d914fd68f04aa683bf473fbef
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
